429ca25edca0ae557c09a51719ebc522af181f27
Mehmet Fatih Uzun <71074814+mfu77@users.noreply.github.com>
2020-09-17 00:47:26 +0300

    update

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-var http = require('http');
-var fs = require('fs');
-
-function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('./index.html', null, function(error, data) {
-    if (error) {
-      response.writeHead(404);
-      response.write('File not found!');
+var server = http.createServer(function (request, response) {
+  fs.readFile('./' + request.url, function(err, data) {
+    if (!err) {
+      var dotoffset = request.url.lastIndexOf('.');
+      var mimetype = dotoffset === -1
+        ? 'text/plain'
+        : {
+          '.html' : 'text/html',
+          '.ico' : 'image/x-icon',
+          '.jpg' : 'image/jpeg',
+          '.png' : 'image/png',
+          '.gif' : 'image/gif',
+          '.css' : 'text/css',
+          '.js' : 'text/javascript'
+        }[ request.url.substr(dotoffset) ];
+      response.setHeader('Content-type' , mimetype);
+      response.end(data);
+      console.log( request.url, mimetype );
     } else {
-      response.write(data);
+      console.log ('file not found: ' + request.url);
+      response.writeHead(404, "Not Found");
+      response.end();
     }
-    response.end();
   });
-}
+})
 
-http.createServer(onRequest).listen(8000);
+var express = require('express')
+var app = express()
+
+app.use(express.static('www'));
+
+var server = app.listen(8000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Express app listening at http://%s:%s', host, port)
+
+})
