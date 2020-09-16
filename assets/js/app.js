9d2ead6f620e6ac34099abece62cfeb8f35bd92c
Mehmet Fatih Uzun <71074814+mfu77@users.noreply.github.com>
2020-09-17 01:20:43 +0300

    delete node

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
-
-$(document).ready(function() {
-    $('.navbar-toggler').on('click', function() {
-        $('body').toggleClass('navbar--opened');
-    });
+document.addEventListener("DOMContentLoaded",function(){
+	this.forms[0].switch.addEventListener("change",function(){
+		this.removeAttribute("class");
+	});
 });
+
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
